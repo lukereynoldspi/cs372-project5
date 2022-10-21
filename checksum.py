4bc1a23b449d48fa10d25827b56034071a34225f
Luke Reynolds <lukereynoldspi@gmail.com>
2022-10-21 16:00:22 -0700

    added comments, removed redundant pseudo_header call

--- checksum.py
+++ checksum.py
@@ -1,19 +1,25 @@
-addrs_file = "tcp_addrs_"
-data_file = "tcp_data_"
+ADDRS_FILE = "tcp_addrs_"
+DATA_FILE = "tcp_data_"
 ZERO_BYTE = b'0x00'
 PTCL = b'0x06'
 
 def tcp_pseudo_header(file_number):
-    pseudo_header = b''
-    f = open(addrs_file + str(file_number) + ".txt", "r")
+
+    # Reads tcp_addr file, strips the ip addresses and returns them as source and destination bytestrings
+    f = open(ADDRS_FILE + str(file_number) + ".txt", "r")
     f = f.read()
     ip_addresses = f.rstrip().split(" ")
     source_bytestring, destination_bytestring = tcp_bytestrings(ip_addresses[0], ip_addresses[1])
-    pseudo_header = pseudo_header + source_bytestring + destination_bytestring + ZERO_BYTE + PTCL
 
-    with open(data_file + str(file_number) + ".dat", "rb") as fp:
+    # Gets tcp header length from tcp_data files
+    with open(DATA_FILE + str(file_number) + ".dat", "rb") as fp:
         tcp_data = fp.read()
         tcp_length = len(tcp_data)
+    tcp_length = tcp_length.to_bytes(1, byteorder ='big')
+
+    # Concats source, destination, byte constants, amnd tcp byte length
+    pseudo_header = source_bytestring + destination_bytestring + ZERO_BYTE + PTCL + tcp_length
+    return pseudo_header
 
 def tcp_bytestrings(source, destination):
     source = source.split(".")
